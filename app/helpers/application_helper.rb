726d775d6d7fcb3c943f860d2a14b1c69c7a1e89
mizkmi <ataru.mizukami@gmail.com>
2016-07-23 18:07:39 +0900

    welcome_v1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
-module ApplicationHelper
+module ApplicationHelper 
+end
+def default_meta_tags
+  {
+    site: 'My WebApp',
+    separator: '-',
+    title: 'mypage',
+    charset: 'utf-8'
+  }
 end
